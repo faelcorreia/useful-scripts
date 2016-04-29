e4da1ba76562236687bae330f850d33fe799da8a
Rafael Correia <rafaelnominato@gmail.com>
2016-04-29 14:39:48 -0300

    blobs-to-files commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## Useful Scripts
+------
+### List of scripts
+
+#### 1. blobs-to-files
+
+Converts the CSV with blobs exported from SQL Squirrel to the files. The CSV must have only one column, with header and every blob must be in one line.
+
+**Usage**
+
+`node index.js filename`
---
+++ blobs-to-files/index.js
@@ -0,0 +1,31 @@
+﻿fs = require('fs')
+fileType = require('file-type')
+
+if (process.argv.length == 3) {
+	var file = process.argv[2]
+	fs.readFile(file, 'utf8', function (err,data) {
+		if (err) {
+			return console.log(err)
+		}
+		if (!fs.existsSync('out')){
+			fs.mkdirSync('out')
+		}
+		var result = data.split('\n')
+		var strs = []
+		for (var i = 1; i < result.length - 1; i++) {
+			strs.push(result[i].replace(/ /g, ''))
+		}
+		for (var i = 0; i < strs.length; i++) {
+			var b = new Buffer(strs[i].length/2)
+			for(var j = 0; j < strs[i].length-1; j+=2) {
+				b[j/2] = (parseInt(strs[i].substr(j, 2), 16))
+			}
+			var wstream = fs.createWriteStream('out/file' + i + '.' + fileType(b).ext)
+			wstream.write(b)
+			wstream.end()
+		}	
+	})
+}
+else {
+	console.log('Usage: node index.js filename')
+}
