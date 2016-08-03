dea7b5f0efc2a985a5c813d08a2933e431b6d07e
Rafael Correia <rafaelnominato@gmail.com>
2016-08-03 19:05:09 -0300

    better report interface

--- isa-export-parser/template/js/app.js
+++ isa-export-parser/template/js/app.js
@@ -1,6 +1,58 @@
 'use strict';
 
-angular.module('isa-export-parser', [])
+angular.module('isa-export-parser', ['ui.router'])
+
+.config(['$stateProvider', '$urlRouterProvider',
+    function($stateProvider, $urlRouterProvider) {
+
+        $stateProvider
+
+            .state('app', {
+            url: '/app',
+            templateUrl: 'views/app.html',
+            controller: 'AppCtrl'
+        })
+
+        .state('app.section', {
+            url: '/section/:sectionId',
+            templateUrl: 'views/section.html',
+            controller: 'SectionCtrl'
+        })
+
+        $urlRouterProvider.otherwise('/app')
+    }
+])
+
+.constant("sections", {
+    data: [{
+        name: "Address Ranges",
+        url: "parsed_addressranges.json",
+    }, {
+        name: "Computer Sets",
+        url: "parsed_computersets.json",
+    }, {
+        name: "Computers",
+        url: "parsed_computers.json",
+    }, {
+        name: "Domain Name Sets",
+        url: "parsed_domainnamesets.json",
+    }, {
+        name: "Enterprise Networks",
+        url: "parsed_enterprisenetworks.json",
+    }, {
+        name: "Protocols",
+        url: "parsed_protocols.json",
+    }, {
+        name: "Proxy Schedule Templates",
+        url: "parsed_proxyscheduletemplates.json",
+    }, {
+        name: "URL Sets",
+        url: "parsed_urlsets.json",
+    }, {
+        name: "User Sets",
+        url: "parsed_usersets.json",
+    }]
+})
 
 .factory('Parseds', ['$http',
     function($http) {
@@ -15,21 +67,19 @@ angular.module('isa-export-parser', [])
     }
 ])
 
-.controller('AppCtrl', ['$scope', 'Parseds',
-    function($scope, Parseds) {
-        $scope.sections = [{
-            name: "URL Sets",
-            url: "parsed_urlsets.json",
-        }, {
-            name: "User Sets",
-            url: "parsed_usersets.json",
-        }]
+.controller('AppCtrl', ['$scope', 'sections',
+    function($scope, sections) {
+        $scope.sections = sections.data
+    }
+])
 
-        $scope.sections.forEach(function(section) {
-            Parseds.get(section.url, function(data) {
-                section.header = data[0]
-                section.data = data.slice(1, data.length)
-            })
+.controller('SectionCtrl', ['$scope', '$stateParams', 'Parseds', 'sections',
+    function($scope, $stateParams, Parseds, sections) {
+        var section = sections.data[$stateParams.sectionId]
+        Parseds.get(section.url, function(data) {
+            section.header = data[0]
+            section.data = data.slice(1, data.length)
         })
+        $scope.section = section
     }
 ])
